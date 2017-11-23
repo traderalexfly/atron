bdb8189cf7ca1a8ba973c38ab322f9aba4951263
Hiago Silva Souza <hiasilva@gmail.com>
2017-11-23 15:25:47 -0200

    Mostrando versão no packages.json

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
 #!/usr/bin/env node
 var app = {
-    chalk       : require('chalk'),
-    clear       : require('clear'),
-    figlet      : require('figlet'),
-    program     : require('commander'),
-    co          : require('co'),
-    fileutils   : require('./utils/file.utils')(),
-    prompt      : require('co-prompt')
+  chalk: require("chalk"),
+  clear: require("clear"),
+  figlet: require("figlet"),
+  program: require("commander"),
+  co: require("co"),
+  fileutils: require("./utils/file.utils")(),
+  prompt: require("co-prompt")
 };
 
-  app.clear();
-  console.log(app.chalk.bold.red(app.figlet.textSync('eTron', { horizontalLayout: 'full' })));
+var pjson = require("./package.json");
 
-  var files = app.fileutils._getAllFilesFromFolder(__dirname + '/command/');
+app.clear();
+console.log(
+  app.chalk.bold.redBright(
+    app.figlet.textSync("eTron", { horizontalLayout: "full" })
+  ),
+  app.chalk.bold.yellowBright(pjson.version)
+);
 
-  for(let i=0; i < files.length; i++){
-    try {
-      var memCo = require(files[i])(app);
-      app.program.option(memCo.command+', '+memCo.flag, memCo.description);
-    } catch(e) {
-      console.log(app.chalk.red('Erro: ' + e));
-    }
+var files = app.fileutils._getAllFilesFromFolder(__dirname + "/command/");
+
+for (let i = 0; i < files.length; i++) {
+  try {
+    var memCo = require(files[i])(app);
+    app.program.option(memCo.command + ", " + memCo.flag, memCo.description);
+  } catch (e) {
+    console.log(app.chalk.red("Erro: " + e));
   }
+}
 
-  app.program.parse(process.argv);
-  
-  //bootstrap do comando.
-  for(let i=0; i < files.length;i++){
-    var memCo = require(files[i])(app);    
-    let fu = eval('app.program.'+memCo.flag.replace('--',''));
-    if(fu){
-        memCo.bootstrap();
-        return;
-    }
+app.program.parse(process.argv);
+
+//bootstrap do comando.
+for (let i = 0; i < files.length; i++) {
+  var memCo = require(files[i])(app);
+  let fu = eval("app.program." + memCo.flag.replace("--", ""));
+  if (fu) {
+    memCo.bootstrap();
+    return;
   }
+}
 
-  //caso não digite nenhum opção eu mostro o helper
-  if(!app.program.args.length) {
-    app.program.help();
+//caso não digite nenhum opção eu mostro o helper
+if (!app.program.args.length) {
+  app.program.help();
 }
